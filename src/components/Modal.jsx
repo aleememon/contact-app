55b0d781270d12ef2c6f2b144e935515493e635d
aleememon <am7359681@gmail.com>
2024-09-04 23:27:58 +0500

    Your commit message here

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { createPortal } from "react-dom";
 import { IoMdClose } from "react-icons/io";
+
 const Modal = ({ onClose, isOpen, children }) => {
+  const handleClick = (event) => {
+    if (event.target.classList.contains("backdrop")) {
+      onClose();
+    }
+  };
+
   return createPortal(
     <>
       {isOpen && (
         <div
-          onClick={onClose}
+          onClick={handleClick}
           className="grid place-items-center backdrop-blur h-screen z-40 w-screen absolute top-0"
         >
           <div className="m-auto relative z-50 min-h-[200px] min-w-[80%] md:max-w-[25%] bg-white p-3">
@@ -18,7 +25,7 @@ const Modal = ({ onClose, isOpen, children }) => {
             </div>
             {children}
           </div>
-          <div className="backdrop-blur h-screen z-40 w-screen absolute top-0" />
+          <div className="backdrop-blur h-screen z-40 w-screen absolute top-0 backdrop" />
         </div>
       )}
     </>,
